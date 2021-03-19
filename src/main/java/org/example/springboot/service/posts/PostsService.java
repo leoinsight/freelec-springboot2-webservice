2e31a6b15fe13ecb59391acbce2a162c64574600
Leo Kwon <mtkwnkr@gmail.com>
2021-03-20 00:17:22 +0900

    등록 기능, 테스트 코드 작성

---
+++ src/main/java/org/example/springboot/service/posts/PostsService.java
@@ -0,0 +1,17 @@
+package org.example.springboot.service.posts;
+
+import lombok.RequiredArgsConstructor;
+import org.example.springboot.domain.posts.PostsRepository;
+import org.example.springboot.web.dto.PostsSaveRequestDto;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
